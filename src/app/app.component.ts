46fce27b890b7190bf57f1d61c2262e9db237e0f
Nivedhitha-JK <nivedhithajk8@gmail.com>
2024-02-10 19:33:43 +0530

    directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,28 @@ export class AppComponent implements OnInit{
   // title = 'To-Do-List-Angular';
   title = 'My Task List';
   taskList: string []=[];
-  ngOnInit(){
-    if(localStorage.getItem('my_tasks') != null){
+info:any=[
+  {fName:'Arjun',age:'22',country:'UK'},
+  {fName:'Vikram',age:'24',country:'India'},
+  {fName:'Vijay',age:'26',country:'France'},
+  {fName:'Raghav',age:'27',country:'Europe'}
+
+];
+
+// info:any=null;
+// day= new Date().getDay();
+
+
+// console.log(day);
+day:number | any;
+isSuccess:boolean = false;
+isDanger:boolean = false;
+isWarning:boolean = false;
+message:any;
+ngOnInit(){
+   this.day = new Date().getDay();
+  console.log(this.day);  
+  if(localStorage.getItem('my_tasks') != null){
     var data = localStorage.getItem('my_tasks');
     if(data != null){
       this.taskList = JSON.parse(data);
@@ -20,6 +40,32 @@ export class AppComponent implements OnInit{
   else{
     window.alert('no data found in local storage');
   }
-  
-  }
+ 
+
+}
+success()
+{
+  this.isSuccess = true;
+  this.isDanger = false;
+  this.isWarning = false;
+  this.message='Success Clicked!';
+}
+
+danger()
+{
+  this.isSuccess =false ;
+  this.isDanger = true;
+  this.isWarning = false;
+  this.message='Danger Clicked!';
+}
+warning()
+{
+  this.isSuccess =false ;
+  this.isDanger =false ;
+  this.isWarning = true;
+  this.message='Warning Clicked!';
+}
+
+user = {pName:'ajay',age:'22'};
+// user.age='23';
 }
